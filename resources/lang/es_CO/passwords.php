af4d778367ac8abc95781fa9f0731e4b737a6544
Ángel Kurten <a.kurten@mensajerosurbanos.com>
2019-07-15 18:33:39 -0500

    Initial commit

---
+++ resources/lang/es_CO/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe tener al menos ocho caracteres y coincidir con la confirmación.',
+    'reset' => '¡Su contraseña ha sido restablecida!',
+    'sent' => '¡Recordatorio de contraseña enviado!',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
+
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Bienvenido {{ auth()->user()->name }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <p>
+                        Esta plataforma nace para apoyar a todas aquellas personas que estan aprendiendo Laravel y/o aquellas que quieren aprender conceptos avanzados de desarrollo,
+                        para lograr dicho objetivo estaremos construyendo un proyecto open-source llamado LMS-Laravel, durante el transcurso del desarrollo estaremos aprendiendo los siguientes conceptos:
+                    </p>
+                    <ul>
+                        <li>Principio SOLID</li>
+                        <li>Principio DRY : Don't repeat yourself</li>
+                        <li>Patron Repositorio</li>
+                        <li>Patron Estrategia</li>
+                        <li>Arquitectura Limpia</li>
+                        <li>Pruebas Unitarias</li>
+                        <li>Desarrollo basado en contratos</li>
+                        <li>Principio "Object Parameter"</li>
+                        <li>Git Flow & Feature Flags</li>
+                        <li>Desarrollo Modular</li>
+                    </ul>
+
+                    <p>La metodologia de trabajo sera muy simple, se realizaran talleres via streaming de Lunes a Sabado, la duracion de cada taller sera de 2 horas hasta tener una version 1.0 del LMS.</p>
+                    <p>Este sitio web se ira actualizando a medida que el LMS se vaya desarrollando, la idea principal es convertir esta experiencia en un laboratio real en donde se vayan viendo los resultados del dia a dia y que todos podamos aportar para el desarrollo del LMS.</p>
+                    <p>A cada usuario internamente se le asiganara un codigo de seguridad, esto con el fin de que solo los usuarios registrados puedan ver los streaming y mas adelante solo se podra mantener una sesion abierta por dispositvo.</p>
+                    <p>SOLICITUD: Estoy tratando de brindar una experiencia educativa diferente a lo que regularmente se hace asi que espero poder contar con tu apoyo a la hora de difundir esta idea.</p>
+                    <p>Si quieres realizar una donacion para apoyar el sitio web y su contenido no dudes en escribirme a <a href="mailto:akurten@angelkurten.com">akurten@angelkurten.com</a></p>
+                </div>
+                <div class="fb-comments" data-href="{{ Request::url()}}" data-order-by="social" data-width="100%" data-numposts="10"></div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
