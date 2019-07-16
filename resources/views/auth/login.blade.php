3739bb61c879cd90577aebf0552cb571cd4138f8
Ángel Kurten <a.kurten@mensajerosurbanos.com>
2019-07-15 20:03:19 -0500

    fix text login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('auth.login') }}</div>
+                <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
