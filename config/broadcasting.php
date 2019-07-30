5ac6c3479b450e6c05d7d607b48979a468de8653
AngelKurten <a.kurten@mensajerosurbanos.com>
2019-07-30 11:56:52 -0500

    fix host broadcasting

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => false,
-                'host' => 'laraco.test',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
             ],
