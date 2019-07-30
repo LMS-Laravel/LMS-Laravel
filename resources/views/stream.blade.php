8ad25de0ff695722292a62ef842a0279ddd7ea26
AngelKurten <a.kurten@mensajerosurbanos.com>
2019-07-30 10:29:52 -0500

    fix bug ws and move chat

--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-4">
-                <chat :user="{{ auth()->user() }}"></chat>
-            </div>
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Bienvenido {{ auth()->user()->name }}</div>
@@ -20,6 +17,9 @@
                         <iframe width="100%" height="450px" src="//159.65.221.105:/LiveApp/play.html?name={{ env('STREAM_KEY') }}" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
+            </div>
+            <div class="col-md-4">
+                <chat :user="{{ auth()->user() }}"></chat>
             </div>
         </div>
     </div>
