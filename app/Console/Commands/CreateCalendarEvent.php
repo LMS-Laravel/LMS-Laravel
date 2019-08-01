c375f7c0bf2a16927d579f96bd6006fa73b59cc4
AngelKurten <a.kurten@mensajerosurbanos.com>
2019-07-31 21:23:57 -0500

    fix calendar

--- app/Console/Commands/CreateCalendarEvent.php
+++ app/Console/Commands/CreateCalendarEvent.php
@@ -40,7 +40,7 @@ class CreateCalendarEvent extends Command
     public function handle()
     {
         $event = new Event();
-        $event->name = 'Curso de Laravel Avanzado: Preguntas y Respuestas';
+        $event->name = 'Sesion curso de Laravel Avanzado';
         $event->description = "Recibiste esta invitación por estar regitrado en el curso de Laravel Avanzado http://phpacademy.co";
         $event->startDateTime = Carbon::create(2019, $this->argument('month'), $this->argument('day'), $this->argument('hour'), $this->argument('minutes'), 00);
         $event->endDateTime = Carbon::create(2019, $this->argument('month'), $this->argument('day'), $this->argument('hour') + 2, $this->argument('minutes'), 00);
