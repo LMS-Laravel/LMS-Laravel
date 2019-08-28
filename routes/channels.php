b0d400e60f654fc6b5cfe67f6f7fe844b15b5341
AngelKurten <a.kurten@mensajerosurbanos.com>
2019-08-27 20:37:08 -0500

    bug fix chat

--- routes/channels.php
+++ routes/channels.php
@@ -15,6 +15,6 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('chat', function ($user) {
+Broadcast::channel('chat-{id}', function ($user) {
     return $user;
 });
